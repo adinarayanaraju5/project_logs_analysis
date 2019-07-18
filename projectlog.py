2ec45ed67d8a0bf2c2a87e07eef4be6d4c66f8a4
adinarayanaraju5 <adinarayanaraju5@gmail.com>
2019-07-18 21:44:13 +0530

    Modified code to match pep8 standard

--- projectlog.py
+++ projectlog.py
@@ -1,11 +1,12 @@
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import psycopg2
 import re
 
-DBNAME = "news"
-
+DBNAME = 'news'
 
 # connection to the news database with query_database function
+
 def query_database(query):
     try:
         db = psycopg2.connect(database=DBNAME)
@@ -15,59 +16,70 @@ def query_database(query):
         return fetch
         db.close
     except:
-        print("Error in connecting to the postgreSQL database news")
+        print 'Error in connecting to the postgreSQL database news'
 
 
 # query for question 1. What are the most popular three articles of all time?
-query1 = """SELECT title,count(*) AS views
-          FROM articles JOIN log
-          ON articles.slug=replace(log.path,'/article/','')
-          GROUP BY title
-          ORDER BY views DESC LIMIT 3;"""
 
+query_popular_three = \
+                          """ SELECT title,count(*) AS views
+                          FROM articles JOIN log
+                          ON articles.slug=replace(log.path,'/article/','')
+                          GROUP BY title
+                          ORDER BY views DESC LIMIT 3;"""
 
 # query for question 2. Who are the most popular article authors of all time?
-query2 = """SELECT name,count(*) AS views
-          FROM authors JOIN articles
-          ON authors.id=articles.author JOIN log
-          ON articles.slug=replace(log.path,'/article/','')
-          WHERE status ='200 OK'
-          GROUP BY name
-          ORDER BY views DESC;"""
 
+query_popular_author = \
+                          """SELECT name,count(*) AS views
+                          FROM authors JOIN articles
+                          ON authors.id=articles.author JOIN log
+                          ON articles.slug=replace(log.path,'/article/','')
+                          WHERE status ='200 OK'
+                          GROUP BY name
+                          ORDER BY views DESC;"""
 
-# query for question 3. On which days did more than 1% of requests lead to errors?
-query3 = """SELECT *
-          FROM (SELECT to_char(log.time::date, 'MON DD, YYYY'),round((newtable.total*100.0)/(COUNT(log.time::date)),1) AS percent
-                FROM (SELECT time::date, COUNT(time::date) AS total
-                      FROM log WHERE status != '200 OK'
-                      GROUP BY time::date
-                      ORDER BY total DESC) AS newtable, log
-                      WHERE newtable.time::date = log.time::date
-                      GROUP BY to_char(log.time::date, 'MON DD, YYYY'),newtable.total
-                      ORDER BY percent DESC) AS error
-                      where percent > 1;"""
+# query for question 3. Which days did more than 1% of requests lead to errors?
+
+query_request_errors = \
+                          """SELECT *
+                          FROM (SELECT to_char(log.time::date, 'MON DD, YYYY'),
+                          round((newtable.total*100.0)/(COUNT(log.time::date)),1
+                          ) AS percent
+                          FROM (SELECT time::date, COUNT(time::date) AS total
+                          FROM log WHERE status != '200 OK'
+                          GROUP BY time::date
+                          ORDER BY total DESC) AS newtable, log
+                          WHERE newtable.time::date = log.time::date
+                          GROUP BY to_char(log.time::date, 'MON DD, YYYY'),
+                          newtable.total
+                          ORDER BY percent DESC) AS error
+                          where percent > 1;"""
 
 
 # function to print the query results
 def print_query_results(message_string, query):
     results = query_database(query)
-    print(message_string)
+    print message_string
     for result in results:
         if re.search('error', message_string):
-            print('\t' + str(result[0]) + ' - ' + str(result[1]) + '% error')
+            print '\t' + str(result[0]) + ' - ' + str(result[1]) \
+                + '% error'
         else:
-            print('\t' + str(result[0]) + ' - ' + str(result[1]) + ' views')
+            print '\t' + str(result[0]) + ' - ' + str(result[1]) \
+                + ' views'
 
+question_popular_three = \
+    '1. What are the most popular three articles of all time?'
+question_popular_author = \
+    '2. Who are the most popular article authors of all time?'
+question_request_errors = \
+    '3. On which days did more than 1% of requests lead to errors?'
 
-string1 = "1. What are the most popular three articles of all time?"
-string2 = "2. Who are the most popular article authors of all time?"
-string3 = "3. On which days did more than 1% of requests lead to errors?"
+question_query_maps = [{question_popular_three: query_popular_three},
+                       {question_popular_author: query_popular_author},
+                       {question_request_errors: query_request_errors}]
 
-# list of dictionaries
-lists = [{string1: query1}, {string2: query2}, {string3: query3}]
-
-# iterating key,value pair of dictionary
-for elements in lists:
-    # calling function print_query_results
-    print_query_results("".join(elements.keys()), "".join(elements.values()))
+for questions in question_query_maps:
+    print_query_results(''.join(questions.keys()),
+                        ''.join(questions.values()))
